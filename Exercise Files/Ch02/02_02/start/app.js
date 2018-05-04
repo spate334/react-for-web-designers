96293ac03e0a164ce402f8e185ed398fcb015e32
Sagar Patel <spate334@uic.edu>
2018-05-04 05:45:54 +0000

    finished 2.3

--- Exercise Files/Ch02/02_02/start/app.js
+++ Exercise Files/Ch02/02_02/start/app.js
@@ -1,11 +1,32 @@
 (function() {
   "use strict";
 
-  var ProductCustomizer = React.createElement(
-    "div",
-    { className: "customizer" },
-    "Product customizer will go here"
-  );
+  // var ProductCustomizer = React.createElement(
+  //   "div",
+  //   { className: "customizer" },
+  //   "Product customizer will go here"
+  // );
+  
+  function ProductImage(props){
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product image"
+    });
+  }
+  
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      React.createElement(
+        "div", 
+        {className: "product-image"}, 
+        React.createElement(ProductImage))
+    );
+  }
 
-  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
+  ReactDOM.render(
+    React.createElement(ProductCustomizer), 
+    document.getElementById("react-root")
+    );
 })();
